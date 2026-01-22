3fb02022c3a33e24c1d01ce1cb7a2b6ed1d958da
Shivansh <shivansh.123neekhra@gmail.com>
2026-01-22 21:27:24 +0530

    solve 1400 fortune telling

--- B_Fortune_Telling.cpp
+++ B_Fortune_Telling.cpp
@@ -16,7 +16,66 @@ mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
 
 void Solve() {
     // write solution here
-    
+    int n , x , y;
+    cin >> n >> x >> y;
+    vector<int> arr(n);
+    int odd = 0;
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+        if(arr[i]&1) odd++;
+    }
+    if(y&1){
+        if(odd&1){
+            //parity changes
+            if(x&1){
+                cout << "Bob" << endl;
+                return;
+            }
+            else{
+                cout << "Alice" << endl;
+                return;
+            }
+        }
+        else{
+            //parity not change
+            if(x&1){
+                cout << "Alice" << endl;
+                return;
+            }
+            else{
+                cout << "Bob" << endl;
+                return;
+            }
+        }
+    }
+    else{
+        if(odd&1){
+            //parity changes
+            if(x&1){
+                cout << "Alice" << endl;
+                return;
+            }
+            else{
+                cout << "Bob" << endl;
+                return;
+            }
+        }
+        else{
+            //parity not change
+            if(x&1){
+                cout << "Bob" << endl;
+                return;
+            }
+            else{
+                cout << "Alice" << endl;
+                return;
+            }
+        }
+    }
+
+
+
+
 }
 
 int32_t main() {
